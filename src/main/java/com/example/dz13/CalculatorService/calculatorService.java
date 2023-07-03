e8f74d76f8b684a322c08d4c97988e43367ca6fa
action0709 <action0709@mail.ru>
2023-07-03 15:58:10 +0400

    1

---
+++ src/main/java/com/example/dz13/CalculatorService/calculatorService.java
@@ -0,0 +1,21 @@
+package com.example.dz13.CalculatorService;
+
+public class CalculatorService {
+
+
+        public int plus (int a, int b){
+            return a+b;
+        }
+        public int minus (int a, int b){
+            return a-b;
+        }
+
+        public int multiply(int a, int b){
+            return a*b;
+        }
+        public int divide(int a, int b){
+            return a/b;
+        }
+
+
+    }
